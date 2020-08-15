a35a0c163cba7bf76ad8f4a7723f25eb453e80c0
chrisjsimpson <chris@karmacomputing.co.uk>
2020-08-15 22:16:56 +0100

    add parameter path items/{item_id}

--- app/main.py
+++ app/main.py
@@ -5,3 +5,7 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int):
+    return {"item_id": item_id}

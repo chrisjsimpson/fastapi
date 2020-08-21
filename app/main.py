a8f3d9a78a9f22eda9fa4918fe0674c28dff0aea
chrisjsimpson <chris@karmacomputing.co.uk>
2020-08-21 15:10:44 +0100

    bump fastapi version and add missing dependencies

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from fastapi import FastAPI, Depends, HTTPException, status
+from fastapi import Depends, FastAPI, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from pydantic import BaseModel
 

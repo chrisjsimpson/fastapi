a157876cf6a630695966a3bc6d00eb9183081468
chrisjsimpson <chris@karmacomputing.co.uk>
2020-08-27 00:25:43 +0100

    correct init_db.py import

--- app/app/db/init_db.py
+++ app/app/db/init_db.py
@@ -2,7 +2,7 @@ from sqlalchemy.orm import Session
 
 from app import crud, schemas
 from app.core.config import settings
-from app.db import base  # noqa: F401
+from app.db.database import Base  # noqa: F401
 
 # make sure all SQL Alchemy models are imported (app.db.base) before initializing DB
 # otherwise, SQL Alchemy might fail to initialize relationships properly

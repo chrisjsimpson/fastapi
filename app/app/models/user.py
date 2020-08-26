cf84091b019051e82513cb614bd724c13e8757b9
chrisjsimpson <chris@karmacomputing.co.uk>
2020-08-27 00:14:12 +0100

    added init_db.py and update users model

--- app/app/models/user.py
+++ app/app/models/user.py
@@ -10,11 +10,11 @@ if TYPE_CHECKING:
 
 
 class User(Base):
-    __tablename__ = "users"
+    __tablename__ = "user"
     id = Column(Integer, primary_key=True, index=True)
     # full_name = Column(String, index=True)
     email = Column(String, unique=True, index=True, nullable=False)
     hashed_password = Column(String, nullable=False)
     disabled = Column(Boolean, default=False)
-    # is_superuser = Column(Boolean(), default=False)
+    is_superuser = Column(Boolean(), default=False)
     # items = relationship("Item", back_populates="owner")

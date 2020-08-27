8db6f65b9b03d60ea102faca9d77b68667d8f057
chrisjsimpson <chris@karmacomputing.co.uk>
2020-08-27 16:47:59 +0100

    tag all routes, bring back / route

--- app/app/main.py
+++ app/app/main.py
@@ -11,20 +11,24 @@ app = FastAPI(
 )
 
 
-@app.get("/random")
+@app.get("/random", tags=["misc"])
 async def random():
     from random import random
     return {"random_number": random()}
 
+@app.get("/", tags=["misc"])
+async def hello_world():
+    return {"msg": "See /docs"}
 
-@app.get("/xkcd")
+
+@app.get("/xkcd", tags=["xkcd"])
 async def xkcd_current():
     f = urllib.request.urlopen('http://xkcd.com/info.0.json')
     resp = f.read().decode('utf-8')
     return json.loads(resp)
 
 
-@app.get("/xkcd/{comic_id}")
+@app.get("/xkcd/{comic_id}", tags=["xkcd"])
 async def xkcd_comic(comic_id: int):
     f = urllib.request.urlopen('http://xkcd.com/info.0.json')
     resp = f.read().decode('utf-8')
